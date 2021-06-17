9fa82c898ff6bf02ac970482cd2d4c68dc7f9915
Andrew Masih <armasih@outlook.com>
2021-06-17 20:11:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/* so how is it used: if (condition1) {code that is executed
+ 'if' condition1 is true } else { code 
+ that is excuted if condition1 is false}
+} */
+
+if (2 > 3) {
+
+  console.log("two is greater than three");
+
+} else {
+
+  console.log("two is not greater than three");
+
+}
+
+
+if (2 < 3) {
+
+  console.log("two is less than three");
+
+} else {
+
+  console.log("two is not less than three");
+
+};
+
+
+
+
+/* only one can be executed: either the code in the 'if' statement or the 'else' statement */
